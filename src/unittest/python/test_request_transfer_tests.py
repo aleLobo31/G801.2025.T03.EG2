441baacf541bc0f543ef53638d5a551903273356
Alejandro <100499148@alumnos.uc3m.es>
2025-03-12 13:23:28 +0100

    upgrade: subtest() implemented for function transfer_request

--- src/unittest/python/test_request_transfer_tests.py
+++ src/unittest/python/test_request_transfer_tests.py
@@ -10,35 +10,44 @@ class MyTestCase(unittest.TestCase):
         '''Opens JSON'''
         try:
             with open("../data/f1_test_valid_case.json", encoding="UTF-8", mode="r") as f:
-                test_data_from_iban = json.load(f)
+                test_data_transfer_request = json.load(f)
         except FileNotFoundError:
             raise AccountManagementException("Wrong file path")
         except json.JSONDecodeError:
-            test_data_from_iban = []
-        cls.__test_data_from_iban = test_data_from_iban
+            test_data_transfer_request = []
+        cls.__test_data_transfer_request = test_data_transfer_request
 
-    def test_request_transfer_tc1(self):
-        transfer_code = ""
-        for input_data in self.__test_data_from_iban:
-            if input_data["id_test"] == "TC1":
-                am = AccountManager()
-                transfer_code = am.transfer_request(input_data["from_iban"], input_data["to_iban"],
-                                                    input_data["concept"], input_data["transfer_type"],
-                                                    input_data["date_transfer"], input_data["amount"])
-                self.assertEqual(transfer_code, "f446d5fda12f7d1a1f72e3feb75decfe")
+    def test_f1_OK_cases(self):
+        am = AccountManager()
+        for index, input_data in enumerate(self.__test_data_transfer_request):
+            test_id = "TC"+ str(index + 1)
+            if test_id in ["TC1"]:
+                with self.subTest(test_id):
+                    transfer_code = am.transfer_request(input_data["from_iban"], input_data["to_iban"],
+                                                        input_data["concept"], input_data["transfer_type"],
+                                                        input_data["date_transfer"], input_data["amount"])
+                    self.assertEqual(transfer_code, input_data["expected_result"])
 
-            try:
-                with open("../data/all_transfers.json", encoding="UTF-8", mode="r") as f:
-                    all_transfers = json.load(f)
-            except FileNotFoundError:
-                raise AccountManagementException("Wrong file path")
-            except json.JSONDecodeError:
-                all_transfers = []
-            transfer_found = False
-            for transfer in all_transfers:
-                if transfer["transfer_code"] == transfer_code:
-                    transfer_found = True
-            self.assertTrue(transfer_found)
+                    try:
+                        with open("../data/all_transfers.json", encoding="UTF-8", mode="r") as f:
+                            all_transfers = json.load(f)
+                    except FileNotFoundError:
+                        raise AccountManagementException("Wrong file path")
+                    except json.JSONDecodeError:
+                        all_transfers = []
+
+                    transfer_found = False
+                    for transfer in all_transfers:
+                        if transfer["transfer_code"] == transfer_code:
+                            transfer_found = True
+                    self.assertTrue(transfer_found)
+
+    def test_f1_KO_cases(self):
+        for index, input_data in enumerate(self.__test_data_transfer_request):
+            test_id = "TC"+ str(index + 1)
+            if test_id not in ["TC1"]:
+                with self.subTest(test_id):
+                    pass
 
 if __name__ == '__main__':
     unittest.main()
