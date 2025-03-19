99faedba22d353e308801c98feac0c46cf51e0f8
AlexPMontes <100499978@alumnos.uc3m.es>
2025-03-19 16:52:35 +0100

    feat: added validation of the signature written in the deposit json and fixed some relative paths

--- src/unittest/python/test_f2_deposit_account.py
+++ src/unittest/python/test_f2_deposit_account.py
@@ -13,7 +13,7 @@ class TestDepositIntoAccount(unittest.TestCase):
     def setUpClass(cls):
         lines = []
         try:
-            with open("../data/f2_test_case.txt", encoding="UTF-8", mode="r") as file:
+            with open(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/f2_test_case.txt", encoding="UTF-8", mode="r") as file:
                 for line in file:
                     lines.append(line.strip())
         except FileNotFoundError as e:
@@ -24,7 +24,7 @@ class TestDepositIntoAccount(unittest.TestCase):
 
     def get_store_hash(self):
         try:
-            with open("../data/deposit_values.json", encoding="UTF-8", mode="r") as f:
+            with open(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/deposit_values.json", encoding="UTF-8", mode="r") as f:
                 file_hash = hashlib.md5(f.__str__().encode()).hexdigest()
         except FileNotFoundError:
             file_hash = ""
@@ -38,14 +38,14 @@ class TestDepositIntoAccount(unittest.TestCase):
             if test_id in ["TC1"]:
                 print(test_id + "hola")
                 with self.subTest(test_id):
-                    with open("../data/tmp_test_data.json", encoding="UTF-8", mode="w") as file:
+                    with open(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/tmp_test_data.json", encoding="UTF-8", mode="w") as file:
                         file.write(json.dumps(input_data))
 
                 frozen_time = datetime.now(timezone.utc)
 
                 with freeze_time(frozen_time):
                     am = AccountManager()
-                    deposit_signature = am.deposit_into_account("../data/tmp_test_data.json")
+                    deposit_signature = am.deposit_into_account(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/tmp_test_data.json")
 
                     test_alg = "SHA-256"
                     test_typ = "DEPOSIT"
@@ -61,6 +61,11 @@ class TestDepositIntoAccount(unittest.TestCase):
 
                 self.assertEqual(test_deposit_signature, deposit_signature)
 
+                with open(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/deposit_values.json", encoding="UTF-8", mode="r") as file:
+                    file_read = json.load(file)
+
+                self.assertEqual(test_deposit_signature, file_read["deposit_signature"])
+
     def test_f2_ko(self):
         index = 1
         for input_data in self.__all_f2_test_cases:
@@ -69,13 +74,13 @@ class TestDepositIntoAccount(unittest.TestCase):
             index += 1
             if test_id not in ["TC1"]:
                 with self.subTest(test_id):
-                    with open("../data/tmp_test_data.json", encoding="UTF-8", mode="w") as file:
+                    with open(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/tmp_test_data.json", encoding="UTF-8", mode="w") as file:
                         file.write(json.dumps(input_data))
 
                     deposit_hash_ini = self.get_store_hash()
                     with self.assertRaises(AccountManagementException) as result:
                         am = AccountManager()
-                        deposit_signature = am.deposit_into_account("../data/tmp_test_data.json")
+                        deposit_signature = am.deposit_into_account(str(Path.home()) + "/PycharmProjects/G801.2025.T03.EG2/src/unittest/data/tmp_test_data.json")
                     if test_id in ["TC45", "TC46", "TC67", "TC68", "TC69", "TC70", "TC71", "TC72", "TC73", "TC74", "TC75"]:
                         self.assertEqual(result.exception.message, "KO (Invalid IBAN)")
                     elif test_id in ["TC62", "TC63", "TC76", "TC77", "TC78", "TC79", "TC80", "TC81", "TC82", "TC83", "TC84", "TC85", "TC86", "TC87", "TC88", "TC89", "TC90", "TC91", "TC92"]:
